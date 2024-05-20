582fe90a70204cca4df1533fc8261faa6a17c592
Satyam Kumar <satyamkumar.sk2003@gmail.com>
2024-05-20 20:52:25 +0530

    add comments

--- addnumber.java
+++ addnumber.java
@@ -1,6 +1,11 @@
 // use of do while loop
 
-// ADD TWO NO AND INPUT IS FROM USER SIDE
+/*
+ * DO WHILE LOOP:
+ * Write a program that allows a user to enter two numbers,
+ * and then sums up the two numbers. The user should be able to
+ * repeat this action until they indicate they are done.
+ */
 
 import java.util.Scanner;
 
